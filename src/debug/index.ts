7e4970445528a2235c01d856a2c7c9f8111944df
RobsonTrasel <robson.trasel@gmail.com>
2022-10-06 01:08:14 -0300

    chore: made some change

---
+++ src/debug/index.ts
@@ -0,0 +1,48 @@
+import { Message, MessageId } from '@open-wa/wa-automate';
+import Express from 'express';
+import { filterProperty } from '../helper/objectManipulation';
+import parse from '../lib/parser';
+import modules from '../modules';
+import Mocked from './mock/index';
+import { MockedMessageObject } from './mock/models';
+
+const DebugServer = Express();
+DebugServer.use(Express.json());
+DebugServer.use(Express.urlencoded({ extended: false }));
+
+DebugServer.get('/', async (req, res) => {
+	const queryParamsCommand = req.query.command as string;
+	const parseResult = parse(queryParamsCommand);
+
+	if (parseResult.isError) return res.status(400).send('Invalid command');
+
+
+	const mockedCient = Mocked.getInstance(req, res);
+	const sender = mockedCient.messageObject?.sender as Message['sender'];
+	mockedCient.messageObject = {
+		id: `Debug - Message sent through WebRequest on ${new Date()}` as MessageId,
+		author: 'Postman',
+		sender: {
+			...sender,
+			formattedName: '+5511@974952409',
+		},
+	};
+
+	modules.WebRequest.registerMockedZaplify(mockedCient);
+
+	const { command, method, args } = parseResult.result;
+	const module = modules.WebRequest.getModule(command);
+	if (!module) return;
+
+	module.setRequester();
+	const messageArgs = {
+		...filterProperty(parseResult.result, "args"),
+		...parseResult.result.args
+	}
+
+	await module.callMethod(method, messageArgs, mockedCient.messageObject as Message);
+
+	mockedCient.sendReplyQueue();
+});
+
+export default DebugServer;
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,5 @@
-import { WebRequest } from './registeredModules/webrequest/index'
-import { ZapLify } from './registeredModules/zaplify/index' 
+import WebRequest from './registeredModules/webrequest/index'
+import ZapLify from './registeredModules/zaplify/index' 
 
 export default {
     WebRequest,
